9117cb005903999ae9fa5ebddf0674b6d106acb5
Ritesh Kumar <ritesh.kumar@sedemac.com>
2022-10-23 20:32:14 +0530

    Duck Simulator: Implement the code from the book

---
+++ p18/Duck.java
@@ -0,0 +1,27 @@
+public abstract class Duck{
+
+    FlyBehaviour flyBehaviour;
+    QuackBehaviour quackBehaviour;
+    public Duck(){
+    }
+
+    public void setFlyBehaviour(FlyBehaviour fb){
+        flyBehaviour = fb;
+    }
+
+    public void setQuackBehaviour(QuackBehaviour qb){
+        quackBehaviour = qb;
+    }
+
+    public abstract void display();
+
+    public void performFly(){
+        flyBehaviour.fly();
+    }
+    public void performQuack(){
+        quackBehaviour.quack();
+    }
+    public void swim(){
+        System.out.println("All ducks float, even decoys!");
+    }
+}
